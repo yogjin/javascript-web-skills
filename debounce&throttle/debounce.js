ad3ab73e903d5f34c152b46d2105eb95ce893a3d
yogjin <youngjin988@gmail.com>
2023-03-05 01:29:22 +0900

    feat: debounce leading edge 구현
    debounce leading edge: 첫번째 요청은 바로 수행하고 그 뒤는 디바운스와 동일하게 동작한다. 
    따라서 debounce leading edge 를 쓰면, throttle 보다 더 성능이 좋은 무한스크롤을 구현할 수 있지 않을까? -> 그냥  Intersection Observer API 쓰면 되기 때문에 debounce, throttle 고민 필요 없음

--- debounce&throttle/debounce.js
+++ debounce&throttle/debounce.js
@@ -15,16 +15,25 @@ const ajaxRequestDebounce = (e) => {
 // 클로저 개념 이용
 // 이전 타이머의 delay 시간이 끝나지 않았을 때 새로운 요청을 하면, 이전 타이머는 취소되고 새로운 요청의 타이머로 교체된다.
 // input에 사용자 입력에 따라 서버에 요청해야할 때 유용하다. (검색어 자동완성, 검색결과 출력 등)
-const debounce = (func, delay) => {
+const debounce = (func, delay, leading = false) => {
   let timer;
 
-  return (e) => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => func(e), delay);
-  };
+  if (leading) {
+    return (e) => {
+      !timer ? func(e) : clearTimeout(timer);
+      timer = setTimeout(() => {
+        timer = null;
+      }, delay);
+    };
+  } else {
+    return (e) => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => func(e), delay);
+    };
+  }
 };
 
 $defaultInput.addEventListener('input', ajaxRequestDefault);
-$debounceInput.addEventListener('input', debounce(ajaxRequestDebounce, 1000));
+$debounceInput.addEventListener('input', debounce(ajaxRequestDebounce, 500));
